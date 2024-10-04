0f18295ab1b47471d80dc3dc775573bf13197464
maffoo <maffoo@users.noreply.github.com>
2024-10-04 16:26:31 +0000

    Upload documentation for current main

---
+++ src-files.js
@@ -0,0 +1,4 @@
+var srcIndex = new Map(JSON.parse('[\
+["numpy",["",[["borrow",[],["mod.rs","shared.rs"]],["npyffi",[],["array.rs","flags.rs","mod.rs","objects.rs","types.rs","ufunc.rs"]]],["array.rs","array_like.rs","convert.rs","datetime.rs","dtype.rs","error.rs","lib.rs","slice_container.rs","strings.rs","sum_products.rs","untyped_array.rs"]]]\
+]'));
+createSrcSidebar();
